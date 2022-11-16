22ac6a38770fb636e3af8d5689e5d2c17b0ad2db
pk1308 <princevkurien@gmail.com>
2022-11-16 17:59:59 +0400

    update readme

--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ import os
 import gdown
 from zipfile import ZipFile
 
-# https://drive.google.com/file/d/1QGH3LozKOmmsM7irmJvCDXW5TTap7nVD/view?usp=sharing
+# https://drive.google.com/file/d/19anC-79_sNyWCzyIx2qUr7ZLOrbiBVln/view?usp=share_link
 
-def get_model_file_gdrive( folder_path : str  = "yolov5.zip"  , folder_id : str = "1QGH3LozKOmmsM7irmJvCDXW5TTap7nVD" ):
+def get_model_file_gdrive( folder_path : str  = "yolov5.zip"  , folder_id : str = "19anC-79_sNyWCzyIx2qUr7ZLOrbiBVln" ):
     if not os.path.exists(folder_path):
         gdown.download(id= folder_id, output= folder_path , quiet=True )
     else:
